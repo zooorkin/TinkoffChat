c8b32b27e9b84de473f648c64ca1b3b42dcfa186
zooorkin <zooorkin@icloud.com>
2018-03-13 19:30:55 +0300

    Возвращён ChangeProfileImageButton.swift

---
+++ TinkoffChat/Profile/ChangeProfileImageButton.swift
@@ -0,0 +1,22 @@
+//
+//  ChangeProfileImageButton.swift
+//  TinkoffChat
+//
+//  Created by Андрей Зорькин on 02.03.18.
+//  Copyright © 2018 Андрей Зорькин. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class ChangeProfileImageButton: UIButton {
+    
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = DesignConstants.profileImageRadius
+        clipsToBounds = true
+        let x = frame.size.width/2
+        if let strongImageView = imageView{
+        strongImageView.frame = CGRect(origin: CGPoint(x: x/2, y: x/2) , size: CGSize(width: x, height: x))
+        }
+    }
+}
